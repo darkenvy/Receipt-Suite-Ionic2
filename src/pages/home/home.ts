22bcad9fa36bdb41abdcdc0d935fdc8b5865f45a
darkenvy <darkenvy6@gmail.com>
2016-11-21 21:09:21 -0800

    minor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,10 +14,12 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 export class HomePage {
   public base64Image: string;
   public base64ImageRaw: string;
+  public debug: string;
   constructor(
     public navCtrl: NavController,
     public http:Http) {
       this.http = http;
+      this.debug = 'pre';
   }
 
 
@@ -26,6 +28,7 @@ export class HomePage {
     // Made for Ionic Serve since we cand access cordova plugins on ionic serve
     this.base64Image = "data:image/jpeg;base64," + CameraImage;
     this.base64ImageRaw = CameraImage;
+    this.debug = 'fake pic took';
   }
 
   takePicture(){
@@ -39,18 +42,27 @@ export class HomePage {
       // this.base64Image = "data:image/jpeg;base64," + imageData;
       this.base64Image = "data:image/jpeg;base64," + imageData;
       this.base64ImageRaw = imageData;
+      this.debug = 'pic took';
     }, (err) => {console.log(err)});
   }
 
   sendPicture() {
+    this.debug = 'send button pressed';
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
     let body = '{"base64": "' + this.base64ImageRaw + '"}'
     console.log('base64Image: ', this.base64Image.length);
-    this.http.post('http://development.com:3000/', body, options)
+    this.http.post('192.168.1.112:3000/', body, options)
+    // this.http.post('http://development.com:3000/', body, options)
       .subscribe(
-        data => console.log(data),
-        error => console.log(error)
+        data => {
+          console.log(data);
+          this.debug = 'pic sent';
+        },
+        error => {
+          console.log(error)
+          this.debug = 'send pic error';
+        }
         )
   }
 
